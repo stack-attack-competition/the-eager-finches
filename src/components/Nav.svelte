d96468ed8a188169b096d0583e8f370991b34c17
Csaba Benkő <benko.csaba89@gmail.com>
2020-02-22 14:28:36 +0100

    touch up + side nav fix

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -7,24 +7,27 @@
 </style>
 
 <div class="android-header mdl-layout__header mdl-layout__header--waterfall">
+	<div aria-expanded="false" role="button" tabindex="0" class="mdl-layout__drawer-button">
+		<i class="material-icons">menu</i>
+	</div>
 	<div class="mdl-layout__header-row">
-          <span class="android-title mdl-layout-title">
-<!--            <img class="android-logo-image" src="images/android-logo.png" alt="logo">-->
-          </span>
+        <span class="android-title mdl-layout-title">
+<!--          <img class="android-logo-image" src="images/android-logo.png" alt="logo">-->
+        </span>
 		<!-- Add spacer, to align navigation to the right in desktop -->
 		<div class="android-header-spacer mdl-layout-spacer"></div>
-<!--		<div class="android-search-box mdl-textfield mdl-js-textfield mdl-textfield&#45;&#45;expandable mdl-textfield&#45;&#45;floating-label mdl-textfield&#45;&#45;align-right mdl-textfield&#45;&#45;full-width">-->
-<!--			<label class="mdl-button mdl-js-button mdl-button&#45;&#45;icon" for="search-field">-->
-<!--				<i class="material-icons">search</i>-->
-<!--			</label>-->
-<!--			<div class="mdl-textfield__expandable-holder">-->
-<!--				<input class="mdl-textfield__input" type="text" id="search-field">-->
-<!--			</div>-->
-<!--		</div>-->
+		<!--		<div class="android-search-box mdl-textfield mdl-js-textfield mdl-textfield&#45;&#45;expandable mdl-textfield&#45;&#45;floating-label mdl-textfield&#45;&#45;align-right mdl-textfield&#45;&#45;full-width">-->
+		<!--			<label class="mdl-button mdl-js-button mdl-button&#45;&#45;icon" for="search-field">-->
+		<!--				<i class="material-icons">search</i>-->
+		<!--			</label>-->
+		<!--			<div class="mdl-textfield__expandable-holder">-->
+		<!--				<input class="mdl-textfield__input" type="text" id="search-field">-->
+		<!--			</div>-->
+		<!--		</div>-->
 		<!-- Navigation -->
 		<div class="android-navigation-container">
 			<nav class="android-navigation mdl-navigation">
-				<a class="mdl-navigation__link mdl-typography--text-uppercase" href="/challenge">Challenges</a>
+				<a class="mdl-navigation__link mdl-typography--text-uppercase" href="/challenges">Challenges</a>
 			</nav>
 		</div>
 		<!-- if logger in -->
@@ -43,8 +46,8 @@
 		<!-- visitor -->
 		<div class="android-navigation-container">
 			<nav class="android-navigation mdl-navigation">
-				<a class="mdl-navigation__link mdl-typography--text-uppercase" href="/challenge">Register</a>
-				<a class="mdl-navigation__link mdl-typography--text-uppercase" href="/challenge">Login</a>
+				<a class="mdl-navigation__link mdl-typography--text-uppercase" href="/challenges">Register</a>
+				<a class="mdl-navigation__link mdl-typography--text-uppercase" href="/challenges">Login</a>
 			</nav>
 		</div>
 	</div>
@@ -55,7 +58,10 @@
 <!--          <img class="android-logo-image" src="images/android-logo-white.png" alt="logo">-->
         </span>
 	<nav class="mdl-navigation">
-		<a class="mdl-navigation__link" href="/challenge">Challenges</a>
+		<a class="mdl-navigation__link" href="/my-profile">User Profile</a>
+		<a class="mdl-navigation__link" href="/my-challenges">My Challenges</a>
+		<a class="mdl-navigation__link" href="/my-bets">My Bets</a>
+		<a class="mdl-navigation__link" href="/logout">Logout</a>
 		<div class="android-drawer-separator"></div>
 	</nav>
 </div>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,14 +5,6 @@
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
 </style>
 
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
